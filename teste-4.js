6a7cbc5f2b1147caf85f35f67778bf739b860c95
Diego Alexandre <diegoalexandre_@live.com>
2024-10-31 00:19:15 -0300

    Respondido questão 4.

--- teste-4.js
+++ teste-4.js
@@ -8,3 +8,23 @@
 
 Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.  
 */
+
+
+const faturamento = [
+    { estado: "SP", valor: 67836.43 },
+    { estado: "RJ", valor: 36678.66 },
+    { estado: "MG", valor: 29229.88 },
+    { estado: "ES", valor: 27165.48 },
+    { estado: "Outros", valor: 19849.53 }
+];
+
+function calculePercentual(array){
+    const valorTotal = array.reduce((acc, item) => acc + item.valor, 0);
+    const participacao = array.map(item => ({
+        estado: item.estado,
+        percentual: (item.valor / valorTotal * 100).toFixed(2) + "%"
+    }));
+    return participacao;
+}
+
+console.table(calculePercentual(faturamento));
